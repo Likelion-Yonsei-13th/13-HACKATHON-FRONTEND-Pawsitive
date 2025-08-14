6fe7c11cbe443960f84ee2b8e3c3a02aa97fb74c
LeeYonDu <gj5051129@gmail.com>
2025-08-14 13:45:53 +0900

    feat/메인페이지 버튼 생성 및 페이지 연결, 네브바/메뉴탭 구현

--- src/app/(pages)/(mainPages)/home/components/sideBar.tsx
+++ src/app/(pages)/(mainPages)/home/components/sideBar.tsx
@@ -8,32 +8,29 @@ interface SidebarProps {
 export default function SideBar({ isOpen }: SidebarProps) {
   return (
     <aside
-      className={`absolute top-0 left-0 h-full bg-white shadow-lg z-30 transition-transform duration-300 ease-in-out ${
+      className={`absolute top-0 left-0 h-full bg-white z-30 transition-transform duration-300 ease-in-out ${
         isOpen ? "translate-x-0" : "-translate-x-full"
       }`}
-      style={{ width: "250px" }}
+      style={{ width: "400px" }}
     >
-      <div className="p-6 ">
+      <div className="p-6">
         <ul className="space-y-6">
-          <li>
-            <Link
-              href="/"
-              className="text-gray-700 border- hover:text-blue-500"
-            >
+          <li className="text-lg font-semibold text-gray-800 border-b-2 pb-4 mb-4">
+            <Link href="/" className="text-gray-700 hover:text-blue-500">
               NestOn 홈
             </Link>
           </li>
-          <li>
+          <li className="text-lg text-gray-800 border-b-2 pb-4 mb-4">
             <Link href="/mypage" className="text-gray-700 hover:text-blue-500">
               마이페이지
             </Link>
           </li>
-          <li>
+          <li className="text-lg text-gray-800 border-b-2 pb-4 mb-4">
             <Link href="/" className="text-gray-700 hover:text-blue-500">
               나의 지역
             </Link>
           </li>
-          <li>
+          <li className="text-lg text-gray-800 border-b-2 pb-4 mb-4">
             <Link href="/" className="text-gray-700 hover:text-blue-500">
               나의 관심 행사 / 상권
             </Link>
