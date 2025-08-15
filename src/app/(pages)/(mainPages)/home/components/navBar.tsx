e1edb4b6ec7461c93a9e342a8d92a48f9011abc5
LeeYonDu <gj5051129@gmail.com>
2025-08-15 23:11:11 +0900

    fix/메뉴탭 디자인 적용

--- src/app/(pages)/(mainPages)/home/components/navBar.tsx
+++ src/app/(pages)/(mainPages)/home/components/navBar.tsx
@@ -9,17 +9,17 @@ interface NavBarProps {
 
 export default function NavBar({ onMenuClick, pageTitle }: NavBarProps) {
   return (
-    <nav className="relative mx-auto min-w-[320px] max-w-[500px] w-full flex flex-col bg-gray-200">
-      <div className="h-16 px-6 w-full flex flex-row justify-between items-center relative z-40">
+    <nav className="relative mx-auto min-w-[320px] max-w-[500px] w-full flex flex-col bg-white">
+      <div className="h-16 px-6 py-10 w-full flex flex-row justify-between items-center relative z-40">
         <button onClick={onMenuClick}>
           <img src="/svg/menu.svg" alt="menu" />
         </button>
 
         <Link href="/">
-          <img src="/svg/NestOn.svg" alt="NestOn" className="w-20" />
+          <img src="/svg/NestOn.svg" alt="NestOn" />
         </Link>
         <Link href="/alarm">
-          <img src="/svg/alarm.svg" alt="alarm" className="w-9" />
+          <img src="/svg/alarm.svg" alt="alarm" />
         </Link>
       </div>
 
